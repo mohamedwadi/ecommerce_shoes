81e5ba580ce223a6eef2083335d57e6651b96866
mohamedwadi <mwadib300@gmail.com>
2025-08-23 20:48:50 +0200

    create primary body and header for app in screen home

--- lib/features/shop/screens/home/screens/home.dart
+++ lib/features/shop/screens/home/screens/home.dart
@@ -8,58 +8,23 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(0),
-          child: Column(
-            children: [
-              Stack(
-                children: [
-                  SizedBox(
-                    height: THelperFunctions.screenHeight(),
-                  ),
-                  Positioned(
-                    child: Container(
-                      height: THelperFunctions.screenHeight() / 2.5,
-                      decoration: BoxDecoration(
-                          color: TColors.primary,
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(10),
-                              topRight: Radius.circular(10)
-                          )
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: THelperFunctions.screenHeight() / 2.5 - 20,
-                    bottom: 0,
-                    right: 0,
-                    left: 0,
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: TColors.white,
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(10),
-                              topRight: Radius.circular(10)
-                          )
-                      ),
-                    ),
-                  ),
-                  ShapeCircleWhite(
-                    topPositioned: -THelperFunctions.screenHeight() / 6,
-                    leftPositioned: THelperFunctions.screenWidth() - 150,),
-                  ShapeCircleWhite(
-                    topPositioned:  THelperFunctions.screenHeight() / 11,
-                    leftPositioned: THelperFunctions.screenWidth() - 90,),
-                ],
-              ),
-            ],
-          ),
+        child: Column(
+          children: [
+            TPrimaryHeaderContainer(child:
+            Column(
+              children: [
+                TAppBarHomeScreen(),
+              ],
+            ))
+          ],
         ),
       ),
     );
   }
 }
+
 
 class ShapeCircleWhite extends StatelessWidget {
   const ShapeCircleWhite({
@@ -78,11 +43,61 @@ class ShapeCircleWhite extends StatelessWidget {
         width: THelperFunctions.screenHeight() / 2.5,
         height: THelperFunctions.screenHeight() / 2.5,
         decoration: BoxDecoration(
-            color: TColors.textWhite.withOpacity(0.1),
-            borderRadius: BorderRadius.circular(500)
+          color: TColors.textWhite.withOpacity(0.1),
+          borderRadius: BorderRadius.circular(500),
         ),
       ),
     );
   }
 }
-
+class TPrimaryHeaderContainer extends StatelessWidget {
+  const TPrimaryHeaderContainer({
+    super.key,
+    required this.child
+  });
+  final Widget child;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        SizedBox(height: THelperFunctions.screenHeight()),
+        Positioned(
+          child: Container(
+            height: THelperFunctions.screenHeight() / 2.5,
+            decoration: BoxDecoration(
+              color: TColors.primary,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: THelperFunctions.screenHeight() / 2.5 - 30,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          child: Container(
+            decoration: BoxDecoration(
+              color: TColors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              ),
+            ),
+          ),
+        ),
+        ShapeCircleWhite(
+          topPositioned: -THelperFunctions.screenHeight() / 6,
+          leftPositioned: THelperFunctions.screenWidth() - 150,
+        ),
+        ShapeCircleWhite(
+          topPositioned: THelperFunctions.screenHeight() / 11,
+          leftPositioned: THelperFunctions.screenWidth() - 90,
+        ),
+        child
+      ],
+    );
+  }
+}
